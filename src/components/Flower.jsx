f451b46e94247ceb4012d9bcdbc65757a5fa453d
Aurorien <starlightnoise@gmail.com>
2023-06-05 22:28:13 +0200

    Add rendering of the object with same id as in the routerparam

--- src/components/Flower.jsx
+++ src/components/Flower.jsx
@@ -1,10 +1,32 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+
 function Flower() {
+  const flowerId = useParams(),
+    flowers = useSelector((state) => state.flowers),
+    flower = flowers.find((flower) => flower.id === parseInt(flowerId.id));
+  // console.log("BLOMMAN ID", flowerId.id);
+  // console.log("Blommorna", flowers);
+  // console.log("BLOMMAN", flower);
+
+  const Img = styled.img`
+    max-width: 300px;
+    height: auto;
+  `;
+
   return (
     <>
-    <h1>blomnamn</h1>
-    <h2>blomrecept</h2>
+      {flower && (
+        <>
+          <Img src={flower.img} alt={flower.name} />
+          <h1>{flower.name}</h1>
+          <h2>Smak: {flower.tastenotes.join(", ")}</h2>
+          <h2>Användning: {flower.use.join(". ")}</h2>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default Flower
+export default Flower;
